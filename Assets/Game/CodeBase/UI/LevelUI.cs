21357cdc7465b792086ec830cd4ff0de1208a0ab
Vadim <kripesk2013@yandex.ru>
2025-09-26 13:34:18 +0300

    Add new gameplay and menu UI art assets
    Added and updated multiple UI sprites and meta files for gameplay and main menu, including Bomb, Delete, Mix, Swap, Pause, Rating, and container buttons, as well as backgrounds and reference images. Removed obsolete planet art and updated Unity scene and prefab references to support new UI elements.

--- Assets/Game/CodeBase/UI/LevelUI.cs
+++ Assets/Game/CodeBase/UI/LevelUI.cs
@@ -6,14 +6,35 @@ namespace UI
 {
     public class LevelUI : MonoBehaviour
     {
+        [Header("Abilitys")]
         [SerializeField, ChildGameObjectsOnly] private Button _bombButton;
         [SerializeField, ChildGameObjectsOnly] private Button _swapTwoObjectsButton;
         [SerializeField, ChildGameObjectsOnly] private Button _mixAllObjectsButton;
         [SerializeField, ChildGameObjectsOnly] private Button _deleteObjectButton;
-    
+        [Header("Other Buttons")]
+        [SerializeField, ChildGameObjectsOnly] private Button _hintButton;
+        [SerializeField, ChildGameObjectsOnly] private Button _exitButton;
+        [Header("EndUI")]
+        [SerializeField, ChildGameObjectsOnly] private Button _retryGameButton;
+        [SerializeField, ChildGameObjectsOnly] private Button _exitFromEndUIButton;
+        
         public Button BombButton => _bombButton;
         public Button SwapTwoObjectsButton => _swapTwoObjectsButton;
         public Button MixAllObjectsButton => _mixAllObjectsButton;
         public Button DeleteObjectButton => _deleteObjectButton;
+        public Button HintButton => _hintButton;
+        public Button ExitButton => _exitButton;
+        public Button RetryGameButton => _retryGameButton;
+        public Button ExitFromEndUIButton => _exitFromEndUIButton;
+    }
+
+    public class LevelUIPresenter
+    {
+        private readonly LevelUI _levelUI;
+
+        public LevelUIPresenter(LevelUI levelUI)
+        {
+            _levelUI = levelUI;
+        }
     }
 }
